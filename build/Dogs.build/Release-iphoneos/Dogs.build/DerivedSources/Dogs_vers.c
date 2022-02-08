4787ff874a0c725604b12ababb0304c6bba94805
Yusuf Miletli <miletliyusuf@gmail.com>
2022-02-08 15:33:12 +0300

    Reset build

--- build/Dogs.build/Release-iphoneos/Dogs.build/DerivedSources/Dogs_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char DogsVersionString[];
- extern const double DogsVersionNumber;
-
- const unsigned char DogsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Dogs  PROJECT:Dogs-1" "\n";
- const double DogsVersionNumber __attribute__ ((used)) = (double)1.;
--- build/Dogs.build/Release-iphoneos/Dogs.build/Objects-normal/arm64/Dogs_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/yusuf/Developer/Personal/FrameworkPractice/Dogs/build/Dogs.build/Release-iphoneos/Dogs.build/DerivedSources/Dogs_vers.c
